7e7812156a1ea1f1741f5b15b705bb34160f7a03
Fuato1 <fuatojfj@gmail.com>
2018-09-10 08:41:12 -0300

    Second commit, (added listing of files and directories inside a folder and colors)

--- main.go
+++ main.go
@@ -4,6 +4,29 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
 
+	"github.com/fatih/color"
+)
+
+func main() {
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if file.Mode().IsDir() {
+			fn := file.Name()
+			cl := color.New(color.FgCyan)
+
+			cl.Printf("%s     ", fn)
+		} else if file.Mode().IsRegular() {
+			fmt.Printf("%s     ", "file")
+		}
+	}
+	fmt.Println()
 }
