34be964f4ff5a5299f67df2d7acbdcb5c971b296
Fuato1 <fuatojfj@gmail.com>
2018-09-11 12:19:02 -0300

    Productive version, added two flags for only list directories (-o=d) or files (-o=f), or with no flag both

--- main.go
+++ main.go
@@ -5,28 +5,58 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/fatih/color"
 )
 
 func main() {
-	files, err := ioutil.ReadDir(".")
+	arg := flag.String("o", "all", "Lists only directories (d) or files (f)")
+	flag.Parse()
+
+	files := readFiles(".")
+
+	ls(files, *arg)
+}
+
+func readFiles(dir string) []os.FileInfo {
+	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _, file := range files {
-		if file.Mode().IsDir() {
-			fn := file.Name()
-			cl := color.New(color.FgCyan)
+	return files
+}
 
-			cl.Printf("%s     ", fn)
-		} else if file.Mode().IsRegular() {
-			fmt.Printf("%s     ", "file")
+func ls(files []os.FileInfo, arg string) {
+	for _, file := range files {
+		if strings.Split(file.Name(), "")[0] == "." {
+			continue
+		} else if arg != "all" {
+			if arg == "d" && file.Mode().IsDir() {
+				fn := file.Name()
+				cl := color.New(color.FgCyan)
+
+				cl.Printf("%s     ", fn)
+			} else if arg == "f" && file.Mode().IsRegular() {
+				fmt.Printf("%s     ", file.Name())
+			}
+		} else if arg == "all" {
+			if file.Mode().IsDir() {
+				fn := file.Name()
+				cl := color.New(color.FgCyan)
+
+				cl.Printf("%s     ", fn)
+			} else if file.Mode().IsRegular() {
+				fmt.Printf("%s     ", file.Name())
+			}
 		}
 	}
+
 	fmt.Println()
 }
